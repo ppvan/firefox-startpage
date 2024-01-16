0b910d2c85125904040bd6cc2b9780cf6cabd8ff
ppvan <phuclaplace@gmail.com>
2024-01-16 08:51:16 +0700

    Update shortcut

--- web/js/config.js
+++ web/js/config.js
@@ -1,45 +1,55 @@
 const CONFIG = {
-    name: "ppvan",
+  name: "ppvan",
 
-    // link of bottom menus (dynamic links)
-    bottomLinks: [
-        { name: "Notion", link: "https://www.notion.so" },
-        { name: "Calendar", link: "https://calendar.google.com" },
-    ],
+  // link of bottom menus (dynamic links)
+  bottomLinks: [
+    { name: "Notion", link: "https://www.notion.so" },
+    { name: "Calendar", link: "https://calendar.google.com" },
+  ],
 
-    // link of card menus (ststic links max: 4 list)
-    cardItem: [
+  // link of card menus (ststic links max: 4 list)
+  cardItem: [
+    {
+      title: "Mạng xã hội",
+      links: [
+        { name: "Youtube", link: "https://youtube.com" },
+        { name: "Facebook", link: "https://www.facebook.com/" },
+        { name: "Github", link: "https://github.com/ppvan" },
+        { name: "Reddit", link: "https://www.reddit.com/" },
+      ],
+    },
+    {
+      title: "Ứng dụng Google",
+      links: [
+        { name: "Gmail", link: "https://mail.google.com" },
+        { name: "Docs", link: "https://docs.google.com/document/u/0/" },
+        { name: "Drive", link: "https://drive.google.com/" },
+        { name: "Forms", link: "https://docs.google.com/forms/u/0/" },
+      ],
+    },
+    {
+      title: "Work Space",
+      links: [
+        { name: "Mail VNU", link: "https://mail.google.com/mail/u/2/#inbox" },
+        { name: "Mail Công ty", link: "https://pro48.emailserver.vn:2096/" },
+        { name: "Điểm danh", link: "https://tms.blueeye.ai/timesheet" },
         {
-            title: "Mạng xã hội", links: [
-                { name: "Youtube", link: "https://youtube.com" },
-                { name: "Facebook", link: "https://www.facebook.com/" },
-                { name: "Github", link: "https://github.com/ppvan" },
-                { name: "Reddit", link: "https://www.reddit.com/" },
-            ]
+          name: "Jira",
+          link: "https://jira.seta-international.vn/secure/WelcomeToJIRA.jspa",
         },
+      ],
+    },
+    {
+      title: "Giải trí",
+      links: [
+        { name: "Anime", link: "https://animehay.city/" },
+        { name: "Hako", link: "https://ln.hako.vn/" },
         {
-            title: "Ứng dụng Google", links: [
-                { name: "Gmail", link: "https://mail.google.com" },
-                { name: "Docs", link: "https://docs.google.com/document/u/0/" },
-                { name: "Drive", link: "https://drive.google.com/" },
-                { name: "Forms", link: "https://docs.google.com/forms/u/0/" },
-            ]
+          name: "Quản lý đào tạo",
+          link: "https://daotaodaihoc.uet.vnu.edu.vn/qldt/",
         },
-        {
-            title: "UET Space", links: [
-                { name: "Course", link: "https://courses.uet.vnu.edu.vn" },
-                { name: "Mail", link: "https://mail.google.com/mail/u/2/#inbox" },
-                { name: "Đào tạo", link: "https://daotao.vnu.edu.vn" },
-                { name: "Đăng ký môn", link: "http://dangkyhoc.vnu.edu.vn" }
-            ]
-        },
-        {
-            title: "Giải trí", links: [
-                { name: "Anime", link: "https://animehay.city/" },
-                { name: "Hako", link: "https://ln.hako.vn/" },
-                { name: "Quản lý đào tạo", link: "https://daotaodaihoc.uet.vnu.edu.vn/qldt/" },
-                { name: "Emptyness", link: "#" },
-            ]
-        },
-    ],
-}
+        { name: "Emptyness", link: "#" },
+      ],
+    },
+  ],
+};
